2b2d6c11f2b6d2ac1f7eb289891a703425520ff4
Yaroslav-Bonadarenko <y_bondarenko-itssv@kw.com>
2022-05-20 02:18:22 +0300

    add Dockerfile

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const port = 3000;
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+let Db;
+if (process.env.NODE_ENV==='development') {
+    Db = "mongodb://host.docker.internal:27017/user-db";
+    Db += "?retryWrites=true&w=majority"
+} else {
+    Db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+}
 
-mongoose.connect(DB,{
+mongoose.connect(Db,{
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
 }).then(con=>{
     console.log(con.connections);
     console.log('Db connections successful!!!');
